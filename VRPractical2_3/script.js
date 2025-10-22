0e60a00c14b033317232f19da4872ba9ce65bb61
cara2025 <caraz@nycstudents.net>
2025-10-22 16:09:07 +0000

    Practical2_3

--- VRPractical2_3/script.js
+++ VRPractical2_3/script.js
@@ -10,4 +10,17 @@ window.addEventListener("DOMContentLoaded",function() {
     let tree = new Tree(x, 0 , z);
   }
 
+
+  for (let i = 0; i< 100; i++){
+    let x = rnd(-20,20)
+    let z = rnd(-20,20);
+    let cloud = new Cloud (x, 0, z);
+  }
+
+  for(let i = 0; i < 100; i++){
+    let x = rnd(-20, 20);
+    let z = rnd(-20,20);
+    let house = new House(x, 0, z);
+  }
+
 })
--- VRPractical2_3/tree.js
+++ VRPractical2_3/tree.js
@@ -18,3 +18,58 @@ class Tree{
     scene.append( this.obj )
   }
 }
+
+class Cloud{
+  constructor(x, y, z){
+    this.obj = document.createElement("a-entity");
+
+    let cloud1 = document.createElement("a-sphere");
+    cloud1.setAttribute("radius", 0.5);
+    cloud1.setAttribute("position", "0 5 0");
+    this.obj.append(cloud1);
+    
+    let cloud2 = document.createElement("a-sphere");
+      cloud2.setAttribute("radius", 0.5);
+      cloud2.setAttribute("position", "0.5 5.5 0");
+      this.obj.append(cloud2);
+
+    let cloud3 = document.createElement("a-sphere");
+        cloud3.setAttribute("radius", 0.5);
+        cloud3.setAttribute("position", "1 5 0");
+        this.obj.append(cloud3)
+
+    this.obj.setAttribute("position", {x:x, y:y, z:z});
+    scene.append(this.obj)
+  }
+}
+
+class House{
+  constructor(x, y, z){
+    this.obj = document.createElement("a-entity");
+
+  let roof = document.createElement("a-cylinder");
+  roof.setAttribute("radius", 1);
+  roof.setAttribute("segments-radial", 3);
+  roof.setAttribute("position", "0 1.3 0");
+  roof.setAttribute("rotation", "270 0 0");
+  roof.setAttribute("color", "red");
+  roof.setAttribute("height", 2);
+  roof.setAttribute("width", 1.5);
+
+  this.obj.append(roof);
+
+  let base = document.createElement("a-box");
+  base.setAttribute("height", 2);
+  base.setAttribute("width", 1.5);
+  base.setAttribute("depth", 2);
+  base.setAttribute("color", "red");
+  base.setAttribute("position", "0 0 0");
+
+  this.obj.append(base);
+
+  this.obj.setAttribute("position", {x:x, y:y, z:z});
+  scene.append(this.obj);
+  }
+
+
+}
