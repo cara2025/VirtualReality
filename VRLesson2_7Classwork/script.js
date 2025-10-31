8d3bc33b5ea0580df174012885654f10382e4555
cara2025 <caraz@nycstudents.net>
2025-10-31 16:24:06 +0000

    dudes

--- VRLesson2_7Classwork/script.js
+++ VRLesson2_7Classwork/script.js
@@ -1,16 +1,27 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene, dudeTemplate;
+let dudes = [];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   dudeTemplate = document.querySelector("#dudeTemplate");
 
   //Challenge 4: Create an array of dudes at random locations.
+  for(let i = 0; i < 15; i++){
+    let x = rnd(-20,20);
+    let y = 0;
+    let z = rnd(-20,20);
+    let d = new Dude(x,2,z);
+    dudes.push(d);
+  }
   
   loop();  
 })
 function loop(){
   //Challenge 5: Make all the dudes jump.
+  for(let d of dudes){
+    d.jump();
+  }
   
   window.requestAnimationFrame( loop );
 }
