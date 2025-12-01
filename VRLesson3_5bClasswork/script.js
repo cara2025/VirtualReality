1241101b63fa2d7060cfdc615a394f2f60d8bfdd
cara2025 <caraz@nycstudents.net>
2025-12-01 17:23:51 +0000

    3.5b

--- VRLesson3_5bClasswork/script.js
+++ VRLesson3_5bClasswork/script.js
@@ -7,6 +7,12 @@ window.addEventListener("DOMContentLoaded",function() {
   /* Challenge 1
      Add 10 new PotHole at random locations to the potholes array
   */
+  for(let i=0;i<10;i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    let p = new Pothole(x,z);
+    potholes.push(p);
+  }
   
   setTimeout(loop,100);
 })
@@ -15,7 +21,11 @@ function loop(){
      Go through the potholes array.  If the distance between you and a 
      pothole is less than 1.7, set the camera's drop variable to true. 
   */
-
+  for(let p of potholes){
+    if(distance(camera,p.obj) < 1.7){
+      camera.drop = true;
+  }
+}
   if(camera.drop){
     camera.object3D.position.y -= 0.025;
   }
