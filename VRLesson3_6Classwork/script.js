1641a40494760e980ac6796b34d630b271cf3269
cara2025 <caraz@nycstudents.net>
2025-12-03 17:18:54 +0000

    3.6

--- VRLesson3_6Classwork/script.js
+++ VRLesson3_6Classwork/script.js
@@ -18,8 +18,34 @@ window.addEventListener("DOMContentLoaded",function() {
     6) Rotate the gem in the negatively on the y-axis
     Note: Copy and paste!
   */
+  window.addEventListener("keydown", function(e){
+    if(e.key == "1"){
+      gem.object3D.rotation.z += 0.1;
+    }
+    if(e.key == "2"){
+      gem.object3D.rotation.z += -0.1;
+    }
+    if(e.key == "3"){
+      gem.object3D.rotation.x += 0.1;
+    }
+    if(e.key == "4"){
+      gem.object3D.rotation.x += -0.1;
+    }
+    if(e.key == "5"){
+      gem.object3D.rotation.y += 0.1;
+    }
+    if(e.key == "6"){
+      gem.object3D.rotation.y -= 0.1;
+    }
+
+  })
 
   /*  Challenge 2
      When the user clicks in the window, resets the gem rotation to (0,0,0)
   */ 
+  window.addEventListener("click", function(){
+    gem.object3D.rotation.x = 0;
+    gem.object3D.rotation.y = 0;
+    gem.object3D.rotation.z = 0;
+  })
 })
