d3471a9eb4a6678a01b33e89625a1ac30233c8c6
cara2025 <caraz@nycstudents.net>
2025-12-05 06:00:09 +0000

    Snowman,animations,score,health,time,ammo

---
+++ VRPractical3_3/ammobox.js
@@ -0,0 +1,5 @@
+class AmmoBox{
+    constructor(x,y,z){
+        this.obj = document.createElement("a-entity");
+
+}}
--- VRPractical3_3/script.js
+++ VRPractical3_3/script.js
@@ -1,9 +1,15 @@
 let rnd = (l,u) => Math.random() * (u-l) + l
-let scene, camera, bullet, enemies = [], ammo_boxes = [], ammo_count = 3, enemy_killed = 0;
+let scene, camera, bullet, enemies = [], ammo_boxes = [], ammo_count = 3, enemy_killed = 0, h = 100, t = 160;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   camera = document.querySelector("a-camera");
+  ammoTxt = document.querySelector("#ammo_count");
+  healthTxt = document.querySelector("#health");
+  enemyTxt = document.querySelector("#enemies_killed");
+  gameTxt = document.querySelector("#gameover");
+  timerTxt = document.querySelector("#timer");
+  winTxt = document.querySelector("#youwin");
 
   window.addEventListener("keydown",function(e){
     //User can only fire with they press the spacebar and have sufficient ammo
@@ -12,12 +18,46 @@ window.addEventListener("DOMContentLoaded",function() {
       ammo_count--;
     }
   })
+
+  for(let i = 0; i<10; i++){
+    let x = rnd(-50,50);
+    let y = 1; 
+    let z = rnd(-50,50);
+    let enemy = new Snowman(x,y,z);
+    enemies.push(enemy);
+  }
+
   
   setTimeout(loop,100);
   setTimeout(countdown,100);
 })
 
 function loop(){
+  healthTxt.setAttribute("value", `Health: ${h}`)
+  ammoTxt.setAttribute("value", `Ammo: ${ammo_count}`)
+  enemyTxt.setAttribute("value", `Score: ${enemy_killed}`)
+  for (let enemy of enemies){
+    enemy.walk();
+    if (bullet && distance(bullet.obj, enemy.obj) < 1.5 && !enemy.die){
+      enemy.die = true;
+      enemy_killed++;
+    }
+    if (distance(camera, enemy.obj) < 2){ 
+      if (!enemy.checkHit){
+        h -= 10;
+        enemy.checkHit = true;
+          if (h <= 0 && t > 0){
+            gameTxt.setAttribute("visible", true);
+  } 
+  }
+} else {
+   enemy.checkHit = false;
+}
+}
+  if (enemy_killed >= 10 && t > 0 && h > 0){
+    winTxt.setAttribute("visible", true);
+  }
+
   if(bullet){
     bullet.fire();
   }
@@ -26,6 +66,12 @@ function loop(){
 }
 
 function countdown(){
+  t--;
+  timerTxt.setAttribute("value", `Time: ${t}`);
+  if (t <= 0 && enemy_killed < 10){
+    gameTxt.setAttribute("visible", true);
+    t = 0;
+  }
 
   setTimeout(countdown,1000);
 }
